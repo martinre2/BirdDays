3b82c3c2c2228a98bdb1666b85c2f97e4c823b72
Ivan Rodriguez <ivan.rodriguez.o@gmail.com>
2011-05-29 01:29:28 -0500

    -Recomendaciones de lugares

--- manejadorInfo.php
+++ manejadorInfo.php
@@ -1,12 +1,21 @@
 <?php
 
 session_start();
-include('configuraciones.php');
+define('NUM_RECOMENDACIONES', 3);
+define('RADIO_BUSQUEDA', 15000);
+define('LAT_DEFECTO', 19.42705);
+define('LON_DEFECTO', -99.127571);
+include('funcionesLlamadasApis.php');
 
 $usuario = llamaApiFacebook('https://graph.facebook.com/me');
 $idUsuario = $usuario['id'];
-if(isset($usuario['location']))
-    $_SESSION['ubicacion'] = $usuario['location'];
+$ubicacionUsuario = null;
+if (isset($usuario['location'])) {
+    $idUbicacion = $usuario['location']['id'];
+    $ubicacion = llamaApiFacebook('https://graph.facebook.com/' . $idUbicacion);
+    if (isset($ubicacion['location']['latitude']) && isset($ubicacion['location']['latitude']))
+        $ubicacionUsuario = array('latitud' => $ubicacion['location']['latitude'], 'longitud' => $ubicacion['location']['longitude']);
+}
 $consulta = "SELECT uid,name,birthday_date FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = $idUsuario) ORDER BY birthday_date";
 $consulta = urlencode($consulta);
 $resultados = llamaApiFacebook('https://api.facebook.com/method/fql.query?query=' . $consulta . '&format=JSON', true);
@@ -24,28 +33,38 @@ foreach ($resultados as $resultado) {
         if($mes == date('m'))
             $cumplesMes[] = array(
                                 'nombre' => $resultado['name'],
-                                'dia' => $dia,
-                                'foto' => 'http://graph.facebook.com/' . number_format($resultado['uid'], 0, '.', '') . '/picture?type=normal'
-                                );
+                                'fecha' => $dia . '/' . $mes,
+                                'foto' => 'http://graph.facebook.com/' . number_format($resultado['uid'], 0, '.', '') . '/picture?type=normal',
+                                'perfil' => 'http://www.facebook.com/profile.php?id=' . number_format($resultado['uid'], 0, '.', '')
+            );
     }
 }
+$recomendaciones = generaLugares(count($cumplesMes), $ubicacionUsuario);
 $_SESSION['eventos'] = $eventos;
 $_SESSION['cumplesMes'] = $cumplesMes;
+$_SESSION['lugares'] = $recomendaciones;
 header('location: ' . URL_BASE);
 
-function llamaApiFacebook($url, $parametrosAntes = false) {
-    $curl = curl_init();
-    if ($parametrosAntes == false)
-        curl_setopt($curl, CURLOPT_URL, $url . '?access_token=' . $_SESSION['token_acceso_facebook']);
-    else
-        curl_setopt($curl, CURLOPT_URL, $url . '&access_token=' . $_SESSION['token_acceso_facebook']);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    $respuesta = curl_exec($curl);
-    if ($respuesta == false)
-        echo 'Tenemos problemas, por favor intenta de nuevo';
-    $respuesta = json_decode($respuesta, true);
-    return $respuesta;
+function generaLugares($numResultados, $ubicacionUsuario) {
+    if(isset($ubicacionUsuario)) {
+        $lat = $ubicacionUsuario['latitud'];
+        $lon = $ubicacionUsuario['longitud'];
+    }
+    else {
+        $lat = LAT_DEFECTO;
+        $lon = LON_DEFECTO;
+    }
+    $tiposLugar = array('food', 'drinks', 'coffee');
+    $resultados = array ();
+    foreach ($tiposLugar as $tipoLugar) {
+        $lugares = llamaApiFoursquare('https://api.foursquare.com/v2/venues/explore?limit=' . $numResultados . '&radius=' . RADIO_BUSQUEDA . '&section=' . $tipoLugar . '&ll=' . $lat . ',' . $lon, true);
+        $lugares = $lugares['response']['groups'][0]['items'];
+        foreach ($lugares as $lugar) {
+            $lugar = $lugar['venue'];
+            $resultados[] = array('id' => $lugar['id'], 'nombre' => $lugar['name'], 'perfil' => 'https://foursquare.com/venue/' . $lugar['id']);
+        }
+    }
+    return $resultados;
 }
 
 ?>
--- manejadorJson.php
+++ manejadorJson.php
@@ -2,8 +2,22 @@
 
 session_start();
 header('Content-type: application/json; charset=UTF-8');
-if (isset($_GET['cumplesMes']))
-    echo json_encode ($_SESSION['cumplesMes']);
-else if($_GET['eventos'])
-    echo json_encode ($_SESSION['eventos']);
+include('funcionesLlamadasApis.php');
+
+
+if (isset($_GET['cumplesMes'])) {
+    echo json_encode($_SESSION['cumplesMes']);
+}
+
+if (isset($_GET['eventos'])) {
+    if(isset($_GET['nombreCal']))
+        $nombreCal = html_entity_decode($_GET['nombreCal']);
+    else
+        $nombreCal = 'Calendario de cumpleaños BirdDays';
+    $_SESSION['eventos']['nombre_calendario'] = $nombreCal;
+    echo json_encode($_SESSION['eventos']);
+}
+if (isset($_GET['lugares'])) {
+    echo json_encode($_SESSION['lugares']);
+}
 ?>
